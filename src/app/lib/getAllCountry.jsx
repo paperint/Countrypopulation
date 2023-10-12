6b9c0106869076caf30e335a5ed177835dc92ef7
Nu <nuuseforstudy@gmail.com>
2023-10-12 18:32:32 +0700

    edit: cache

--- src/app/lib/getAllCountry.jsx
+++ src/app/lib/getAllCountry.jsx
@@ -2,8 +2,7 @@ export default async function getAllCountry(year) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API}/AllCountry/api?year=${year}`,
     {
-      next: { revalidate: 600 },
-      // revalidate data every 10 min
+      cache: "force-cache",
     }
   );
 
--- src/app/lib/getCountry.jsx
+++ src/app/lib/getCountry.jsx
@@ -2,8 +2,7 @@ export default async function getCountry(year) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API}/Country/api?year=${year}`,
     {
-      next: { revalidate: 600 },
-      // revalidate data every 10 min
+      cache: "force-cache",
     }
   );
 
